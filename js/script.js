d7b020c5d86e16c1c4924942644b390e69d9262f
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2020-11-10 11:52:05 +0600

    popular books

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+$(document).ready(function () {
 
+
+
+
+
+
+
+
+
+    $('.accordian-writes').click(function (event) {
+
+
+
+        console.log(event.target.id.split('-'));
+
+            if(event.target.id.split('-')[0]==='btn'){
+
+                $("#book-1").attr('src','img/writers/'+event.target.id.split('-')[1]+'-book1.jpg');
+
+
+                $("#book-2").attr('src','img/writers/'+event.target.id.split('-')[1]+'-book2.jpg')
+            }
+    })
+
+
+
+
+
+})
 
 
 
